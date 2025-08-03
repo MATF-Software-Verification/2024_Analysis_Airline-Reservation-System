20c84b071a9d8ab22650643f355f5f253e258733
NataMata <natafil20@gmail.com>
2025-08-03 20:23:40 +0200

    Fixed bash script.

--- clang/clang_format.sh
+++ clang/clang_format.sh
@@ -7,8 +7,6 @@ for file in "$SOURCE_DIR"/*; do
 		FILE_NAME=$(basename "$file")
 		echo "Formatting: $file..."
 		clang-format "$file"  > "${FILE_NAME%.*}_clang-format_output.txt"
-		diff "$file" "${FILE_NAME%.*}_clang-format_output.txt" > "${FILE_NAME%.*}_diff.txt"
-		rm "${FILE_NAME%.*}_clang-format_output.txt"
 		echo -e "Clang formatting done for ${file}. Differences between original and suggested formatting saved in ${FILE_NAME%.*}_diff.txt\n"
 	fi
 done
