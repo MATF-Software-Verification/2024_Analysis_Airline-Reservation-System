8dd77469ed4c6db4b68379a78cae8dbe8cbe58ab
NataMata <natafil20@gmail.com>
2025-09-10 19:29:17 +0200

    Fixed some typing error.

--- cppcheck/run_cppcheck.sh
+++ cppcheck/run_cppcheck.sh
@@ -11,12 +11,12 @@ xdg-open html_report/index.html
 
 
 echo "Starting analysis with cppcheck with different options..."
-cppcheck --enable=all --inconclusive --suppress=missingInclude --xml --xml-version=2 ../Airline-Reservation-System/src 2> cppcheck_output_posible.xml
-echo "Results saved in cppcheck_output_posible.xml file."
+cppcheck --enable=all --inconclusive --suppress=missingInclude --xml --xml-version=2 ../Airline-Reservation-System/src 2> cppcheck_output_possible.xml
+echo "Results saved in cppcheck_output_possible.xml file."
 
-cppcheck-htmlreport --file=cppcheck_output_posible.xml --report-dir=html_report_posible --source-dir="../Airline-Reservation-System/src" --title="Airline Reservation System"
+cppcheck-htmlreport --file=cppcheck_output_possible.xml --report-dir=html_report_possible --source-dir="../Airline-Reservation-System/src" --title="Airline Reservation System"
 
 echo "Opening report in your browser..."
-xdg-open html_report_posible/index.html
+xdg-open html_report_possible/index.html
 
 
